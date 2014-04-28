87e1d0d46f61ac92731348ec0f072667b6d4ace2
Andrew Lorente <hello@andrewlorente.com>
2014-04-27 19:09:53 -0700

    Wow! Such nginx, very upstart, much bloge

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -21,6 +21,13 @@ node default {
     ensure => present,
   }
 
+  user { 'bloge':
+    shell => '/bin/bash',
+    home => '/home/bloge',
+    managehome => true,
+    ensure => present,
+  }
+
   ssh_authorized_key { 'andrewlorente_alorente':
     ensure => present,
     key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA6q+NIMbgehuTQN/LymntUtqorU3qNqN05yXZKwgRYDapn8XLgJA3OGmfEcaMyK+0ry/US4Kl9fTk5H16YZ/ZTRbJfssVvbWSDTBP/ho1hhjpLEgHD3lmhuO27reypg5aawJpQBFzLKHDBUZ/sUOLawP9LVRiPp2Ve2mG+K3R+k6U9b2MaEIF3cfp3FU20wex/M9pce4a+PjmdM4Yvuy0Mn4MfuhpoLRfEBmI1qlwrqvCTeMK4OGufEQXpe+KmjBovMTKx142lm6XSTJ2JRtuOrSGqUaFFyzHSUSjLGwl1zjdjOafsJZvclyTt/dDB1RmX/Fr2YYk+EZN/a33agDJTQ==',
@@ -29,10 +36,31 @@ node default {
     user => 'andrewlorente',
   }
 
+  ssh_authorized_key { 'bloge_alorente':
+    ensure => present,
+    key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA6q+NIMbgehuTQN/LymntUtqorU3qNqN05yXZKwgRYDapn8XLgJA3OGmfEcaMyK+0ry/US4Kl9fTk5H16YZ/ZTRbJfssVvbWSDTBP/ho1hhjpLEgHD3lmhuO27reypg5aawJpQBFzLKHDBUZ/sUOLawP9LVRiPp2Ve2mG+K3R+k6U9b2MaEIF3cfp3FU20wex/M9pce4a+PjmdM4Yvuy0Mn4MfuhpoLRfEBmI1qlwrqvCTeMK4OGufEQXpe+KmjBovMTKx142lm6XSTJ2JRtuOrSGqUaFFyzHSUSjLGwl1zjdjOafsJZvclyTt/dDB1RmX/Fr2YYk+EZN/a33agDJTQ==',
+    name => 'bloge_alorente',
+    type => 'ssh-rsa',
+    user => 'bloge',
+  }
+
   file { ['/u', '/u/apps']:
     ensure => 'directory',
     owner => 'root',
     group => 'root',
+    mode => '0644'
+  }
+
+  file { [
+      '/u/apps/bloge',
+      '/u/apps/bloge/releases',
+      '/u/apps/bloge/shared/',
+      '/u/apps/bloge/shared/pids',
+      '/u/apps/bloge/shared/log',
+    ]:
+    ensure => 'directory',
+    owner => 'bloge',
+    group => 'bloge',
     mode => '0644'
   }
 
@@ -53,12 +81,19 @@ node default {
     nx_worker_connections => 1024,
     nx_client_max_body_size => '4G',
     nx_types_hash_max_size => 2048,
+    nx_server_names_hash_bucket_size => 64,
     nx_gzip => 'on',
   }
   nginx::resource::upstream { 'andrewlorente':
     ensure => present,
     members => [
       'localhost:8000 weight=1',
+    ],
+  }
+  nginx::resource::upstream { 'bloge':
+    ensure => present,
+    members => [
+      'localhost:8001 weight=1',
     ],
   }
   nginx::resource::location { 'andrewlorente':
@@ -74,6 +109,20 @@ node default {
       'X-Forwarded-Proto' => '$scheme',
     },
   }
+  nginx::resource::location { 'bloge':
+    ensure => present,
+    vhost => 'blog.andrewlorente.com',
+    location => '/',
+    match_type => '~',
+    proxy => 'http://bloge',
+    proxy_set_headers => {
+      'REMOTE_ADDR' => '$remote_addr',
+      'HTTP_HOST' => '$http_host',
+      'Host' => '$host',
+      'X-Forwarded-Proto' => '$scheme',
+    },
+  }
+
 
   nginx::resource::vhost { 'andrewlorente-nonssl.com':
     ensure => present,
@@ -81,6 +130,13 @@ node default {
     listen_port => 80,
     force_ssl => true,
     www_root => '/u/apps/andrewlorente/current/static',
+  }
+  nginx::resource::vhost { 'blog.andrewlorente-nonssl.com':
+    ensure => present,
+    server_names => ['blog.andrewlorente.com'],
+    listen_port => 80,
+    force_ssl => true,
+    www_root => '/u/apps/bloge/current/static',
   }
 
   nginx::resource::vhost { 'andrewlorente.com':
@@ -93,6 +149,16 @@ node default {
     ssl_key => '/etc/ssl/andrewlorente.com.key',
   }
 
+  nginx::resource::vhost { 'blog.andrewlorente.com':
+    ensure => present,
+    server_names => ['blog.andrewlorente.com'],
+    listen_port => 443,
+    www_root => '/u/apps/bloge/current/static',
+    ssl => true,
+    ssl_cert => '/etc/ssl/STAR_andrewlorente_com.chained.crt',
+    ssl_key => '/etc/ssl/andrewlorente.com.key',
+  }
+
 
   file { 'andrewlorente-upstart':
     path => '/etc/init/andrewlorente.conf',
@@ -100,6 +166,16 @@ node default {
     # Hardcoding /tmp/supply_drop like this is crappy. I don't know
     # what I should do instead, though. :(
     source => 'file:///tmp/supply_drop/puppet/manifests/andrewlorente.conf',
+    mode => 'a=r,u+w',
+    owner => 'root',
+    group => 'root',
+  }
+  file { 'bloge-upstart':
+    path => '/etc/init/bloge.conf',
+    ensure => file,
+    # Hardcoding /tmp/supply_drop like this is crappy. I don't know
+    # what I should do instead, though. :(
+    source => 'file:///tmp/supply_drop/puppet/manifests/bloge.conf',
     mode => 'a=r,u+w',
     owner => 'root',
     group => 'root',
