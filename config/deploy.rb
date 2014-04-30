5d573f4914c5b03c31f2d542ae95d37511aced62
Andrew Lorente <hello@andrewlorente.com>
2014-04-29 20:25:20 -0700

    Centralize my deploy scripts into one fabfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,32 +1,5 @@
 lock '3.1.0'
 
-set :application, 'andrewlorente'
-set :repo_url, 'git@github.com:AndrewLorente/andrewlorente.git'
-set :scm, :git
-
-set :deploy_to, '/u/apps/andrewlorente'
-
-namespace :deploy do
-  task :compile do
-    on roles(:all) do
-      within release_path do
-        execute './build.sh', as: 'andrewlorente'
-      end
-    end
-  end
-end
-
-namespace :deploy do
-  task :restart do
-    on roles(:all) do
-      sudo('initctl', 'restart', 'andrewlorente')
-    end
-  end
-end
-
-after 'deploy:updated', 'deploy:compile'
-after 'deploy:symlink:release', 'deploy:restart'
-
 namespace :puppet do
   set :puppet_parameters, 'puppet/manifests/site.pp'
   set :puppet_lib, "#{fetch(:puppet_destination)}/puppet/modules"
---
+++ fabfile.py
@@ -0,0 +1,34 @@
+from fabric.api import cd, run, sudo, env, execute
+from datetime import datetime
+
+env.hosts = ['andrewlorente.com']
+apps = {
+    'bloge': ['bloge@andrewlorente.com'],
+    'andrewlorente': ['andrewlorente@andrewlorente.com'],
+}
+
+def deploy(app):
+    if app not in apps.keys():
+        raise Exception("Unknown deploy target '{0}'".format(app))
+
+    release_id = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    execute(build, app, release_id, hosts=apps[app])
+    execute(release, app, hosts=['alorente@andrewlorente.com'])
+
+def build(app, release_id):
+    release_dir = "/u/apps/{0}/releases/{1}".format(app, release_id)
+    repo = "https://github.com/AndrewLorente/{0}.git".format(app)
+    run("git clone -q {0} {1}".format(repo, release_dir))
+
+    with cd(release_dir):
+        run("cabal update")
+        run("cabal install --constraint 'template-haskell installed' "
+            "--dependencies-only --force-reinstall")
+        run("cabal configure")
+        run("cabal build")
+    run("ln -nfs {0} /u/apps/{1}/current".format(release_dir, app))
+
+def release(app):
+    sudo("initctl restart " + app)
+
